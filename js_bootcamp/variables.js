f400dbefb853ecd08fe5e56168289a9dd6ace2c3
Siphiwe Ntombela <mrwie05@gmail.com>
2025-04-30 23:40:48 +0200

    first commit

---
+++ js_bootcamp/variables.js
@@ -0,0 +1,27 @@
+let name = "Siphiwe Ntombela";               
+let age = 24;           
+let Student = true;
+
+console.log("My name is: " + name);  
+console.log("My age is: " + age);  
+console.log("Am I a student? " + isStudent);  
+ 
+let favoriteColour = "blue";  
+console.log("My favorite colour is: blue" + favoriteColour);  
+  
+favoriteColour = "green";  
+console.log("Now my favorite colour is: grey" + favoriteColour);  
+
+let num1 = 15;  
+let num2 = 3;  
+
+let sum = num1 + num2;  
+let difference = num1 - num2;  
+let product = num1 * num2;  
+let quotient = num1 / num2;  
+
+console.log("Arithmetic with", num1, "and", num2);  
+console.log("Sum:", sum);  
+console.log("Difference:", difference);  
+console.log("Product:", product);  
+console.log("Quotient:", quotient);  
