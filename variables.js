a163a186474f70103405dc95fc4e7318770c68c8
Siphiwe Ntombela <mrwie05@gmail.com>
2025-04-30 20:34:59 +0200

    first commit

---
+++ variables.js
@@ -0,0 +1,32 @@
+// Exercise 1: Variable Declaration  
+// Personalized with my details!  
+let name = "Siphiwe Ntombela";               
+let age = 24;                 !  
+let Student = true;          
+
+console.log("My name is: " + name);  
+console.log("My age is: " + age);  
+console.log("Am I a student? " + isStudent);  
+
+// Exercise 2: Variable Reassignment  
+let favoriteColour = "blue";  
+console.log("My favorite colour is: blue" + favoriteColour);  
+
+// Changing my mind about the color!  
+favoriteColour = "green";  
+console.log("Now my favorite colour is: grey" + favoriteColour);  
+
+// Exercise 3: Arithmetic Operations  
+let num1 = 15;  
+let num2 = 3;  
+
+let sum = num1 + num2;  
+let difference = num1 - num2;  
+let product = num1 * num2;  
+let quotient = num1 / num2;  
+
+console.log("Arithmetic with", num1, "and", num2);  
+console.log("Sum:", sum);  
+console.log("Difference:", difference);  
+console.log("Product:", product);  
+console.log("Quotient:", quotient);  
